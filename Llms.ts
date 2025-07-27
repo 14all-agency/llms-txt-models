55bcd04fa4e679a4b5749dbae3cf0958d6b93c0b
MitchJohn <mitchell@kiwisprout.nz>
2025-07-27 14:16:58 +1200

    z

---
+++ Llms.ts
@@ -0,0 +1,61 @@
+import { ObjectId } from 'bson';
+import { z } from "zod";
+import { OrganisationModel, OrganisationModelSchema, OrganisationResult } from './Organisation';
+
+export const QueryResult = z.object({
+  _id: z.instanceof(ObjectId),
+  org: z.union([
+    z.instanceof(ObjectId),
+    OrganisationResult,
+  ]).describe("The owner of this document"),
+  domain: z.string().nullable().optional().describe("The website domain (for faster access)"),
+  createdAt: z.date().nullable().optional(),
+  updatedAt: z.date().nullable().optional().describe("When the file settings were last changed"),
+  generationRequestedAt: z.date().nullable().optional().describe("When the file regen was requested, blank once completed"),
+  lastHit: z.date().nullable().optional().describe("Date of last tracked hit"),
+  totalHits: z.number().nullable().optional().describe("Total amount of tracked hits to file"),
+  totalLinks: z.number().nullable().optional().describe("Total amount of links in the file"),
+  generatedFile: z.string().nullable().optional().describe("The generated file to return"),
+  githubLink: z.string().nullable().optional().describe("The github link (if enabled)"),
+});
+
+export type QueryResultEntity = z.infer<typeof QueryResult>;
+
+export const QueryModelSchema = z.object({
+  id: z.string(),
+  org: z.union([
+    z.string(),
+    OrganisationModelSchema,
+  ]),
+  createdAt: QueryResult.shape.createdAt,
+  updatedAt: QueryResult.shape.updatedAt,
+  generationRequestedAt: QueryResult.shape.generationRequestedAt,
+  lastHit: QueryResult.shape.lastHit,
+  totalHits: QueryResult.shape.totalHits,
+  totalLinks: QueryResult.shape.totalLinks,
+  domain: QueryResult.shape.domain,
+  generatedFile: QueryResult.shape.generatedFile,
+  githubLink: QueryResult.shape.githubLink,
+});
+
+export type QueryModel = z.infer<typeof QueryModelSchema>;
+
+export const QueryModel = {
+  convertFromEntity(entity: QueryResultEntity): QueryModel {
+    const obj: QueryModel = {
+      id: entity._id.toHexString(),
+      // @ts-ignore
+      org: ObjectId.isValid(entity.org) ? entity.org.toHexString() : OrganisationModel.convertFromEntity(entity.org, includeCredentials),
+      ...entity.createdAt && { createdAt: new Date(entity.createdAt || new Date()) },
+      ...entity.updatedAt && { updatedAt: new Date(entity.updatedAt || new Date()) },
+      ...entity.generationRequestedAt && { generationRequestedAt: new Date(entity.generationRequestedAt || new Date()) },
+      ...entity.lastHit && { lastHit: new Date(entity.lastHit || new Date()) },
+      totalHits: entity.totalHits || 0,
+      totalLinks: entity.totalLinks || 0,
+      domain: entity.domain || "",
+      generatedFile: entity.generatedFile || "",
+      githubLink: entity.githubLink || "",
+    };
+    return QueryModelSchema.parse(obj);
+  },
+};
